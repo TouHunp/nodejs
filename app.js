6f4b9ba36c514d8695d6d48b07b634efce7eb8f1
TouHunp <mmilove3755@gmail.com>
2022-10-14 17:26:08 +0800

    api training

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ const calculator = require("./calculator.js");
 console.log(calculator);
 let addResult = calculator.add(1, 2);
 console.log(`1+2=${addResult}`);
+let subtractResult = calculator.subtract(2, 1);
+console.log(`2-1=${subtractResult}`);
--- server.js
+++ server.js
@@ -82,6 +82,22 @@ app.get("/json_data", function (req, res) {
   const data = require("./data.json");
   res.json(data);
 });
+//api http://127.0.0.1:8080/parameters?head=Head&para=paragraph
+app.get("/parameters", function (req, res) {
+  const head_info = req.query.head;
+  const para_info = req.query.para;
+  head_html = `<h1>` + head_info + `</h1>`;
+  paragraph_html = `<p>` + para_info + `</p>`;
+  res.send(head_html + paragraph_html);
+});
+//使用express 解析json文件
+app.use(express.json());
+//接收post json格式請求 返回json格式
+app.post("/handle", function (req, res) {
+  console.log(req.body);
+  res.json(req.body);
+});
+//監聽8080port
 app.listen(8080, () => {
   console.log(`server is running at http://localhost:8080`);
 });
